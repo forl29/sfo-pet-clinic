7328c037af7f2effd316a558753cb3606135e384
Orlando Marquez <orlando042995@gmail.com>
2023-01-24 18:56:13 -0600

    Initial commit

---
+++ src/main/java/springframework/learning/sfopetclinic/SfoPetClinicApplication.java
@@ -0,0 +1,13 @@
+package springframework.learning.sfopetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfoPetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SfoPetClinicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/springframework/learning/sfopetclinic/SfoPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package springframework.learning.sfopetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfoPetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
