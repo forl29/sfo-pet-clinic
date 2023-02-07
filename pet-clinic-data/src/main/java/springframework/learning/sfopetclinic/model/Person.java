9bbca7d5532fb722cbe6dc14ef7f55fc6d7d65c1
Orlando Marquez <orlando042995@gmail.com>
2023-02-07 16:34:02 -0600

    adding base entity, closes #10

--- pet-clinic-data/src/main/java/springframework/learning/sfopetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/springframework/learning/sfopetclinic/model/Person.java
@@ -1,6 +1,6 @@
 package springframework.learning.sfopetclinic.model;
 
-public class Person {
+public class Person extends BaseEntity{
 
     private String firstName;
     private String lastName;
--- pet-clinic-data/src/main/java/springframework/learning/sfopetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/springframework/learning/sfopetclinic/model/PetType.java
@@ -1,6 +1,6 @@
 package springframework.learning.sfopetclinic.model;
 
-public class PetType {
+public class PetType extends BaseEntity{
 
     private String name;
 
