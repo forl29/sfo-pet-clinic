b43ee57f0f8032ebe0d629668ec6f72294e39a97
Orlando Marquez <orlando042995@gmail.com>
2023-02-07 16:22:38 -0600

    Implement Interfaces for services. closes #5

---
+++ pet-clinic-data/src/main/java/springframework/learning/sfopetclinic/services/VetService.java
@@ -0,0 +1,14 @@
+package springframework.learning.sfopetclinic.services;
+
+import springframework.learning.sfopetclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+
+    Vet findById(Long id);
+
+    Vet save(Vet vet);
+
+    Set<Vet> findAll();
+}
