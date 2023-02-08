6a0d3213671bbc23d5c5d71eefcc6c792c53871a
Orlando Marquez <orlando042995@gmail.com>
2023-02-07 18:42:46 -0600

    refactoring to common crud interface. closes #11

--- pet-clinic-data/src/main/java/springframework/learning/sfopetclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/springframework/learning/sfopetclinic/services/PetService.java
@@ -4,11 +4,6 @@ import springframework.learning.sfopetclinic.model.Pet;
 
 import java.util.Set;
 
-public interface PetService {
+public interface PetService extends CrudService<Pet, Long> {
 
-    Pet findById(Long id);
-
-    Pet save(Pet pet);
-
-    Set<Pet> findAll();
 }
